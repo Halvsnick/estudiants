d558ff69e4aeb7d9abeeb1e5f530eb7da9d4015b
Natàlia Cortés Danés <1668991@uab.cat>
2023-10-16 20:22:43 +0200

    PuntInteresBotiga - classe feta i correcte

--- LP-OSM-2223-DLL/PuntInteresBotiga.h
+++ LP-OSM-2223-DLL/PuntInteresBotiga.h
@@ -8,13 +8,44 @@ using namespace std;
 
 class PuntInteresBotiga : public PuntDeInteresBase {
 public:
-    PuntInteresBotiga(Coordinate coord, std::string name, unsigned int color);
+	PuntInteresBotiga() : m_shopTag("") {};
 
-    unsigned int getColor() override;
-    std::string PuntInteresBotiga::getName() {
-        return PuntDeInteresBase::getName(); // Llama al método getName de la clase base.
-    }
+	//contructor per parametres
+	PuntInteresBotiga(Coordinate coord, string name, const string& shopTag, bool wheelchair) :
+		PuntDeInteresBase(coord, name) { m_shopTag = shopTag; m_wheelchair = wheelchair;};
+	
+	unsigned int getColor()
+	{
+		switch (m_wheelchair)
+		{
+		case 1:
+			if (m_shopTag == "bakery")
+				if (getName().find("“Div 06:00-22:00") != string::npos
+					|| getName().find("Dill 06:00-22:00") != string::npos)
+					return 0x4CB944;
+				else
+					return 0xE85D75; 
+		default:
+			if (m_shopTag == "supermarket")
+				return 0xA5BE00;
+			else if (m_shopTag == "tobacco")
+					return 0xFFAD69;
+				else if (m_shopTag == "bakery")
+						return 0xE85D75;
+					else
+						return 0xEFD6AC;
+		}
+	}
+
+	string getName() { return m_shopTag; }
+
+private:
+	string m_shopTag;
+	bool m_wheelchair;
 };
+
+
+
 
 
 
