f1c2b86339e4b77b549ab10d841434594046259c
Natàlia Cortés Danés <1668991@uab.cat>
2023-10-16 20:34:54 +0200

    CamiSolucio - en proces

--- LP-OSM-2223-DLL/CamiSolució.h
+++ LP-OSM-2223-DLL/CamiSolució.h
@@ -13,6 +13,29 @@ public:
     std::vector<std::string> getCamins();  // Agafa tots els camins
 };
 
+//////////////////////////////////////////////////////////////////////
+#include "Common.h"
+#include <vector>
+
+class CamiSolucio {
+public:
+    std::vector<Coordinate> getCamiCoords();
+};
+
+std::vector<Coordinate> CamiSolucio::getCamiCoords() {
+    std::vector<Coordinate> coordenadas;
+
+    // Añadir las coordenadas al vector
+    coordenadas.push_back({ 41.4928803, 2.1452381 });
+    coordenadas.push_back({ 41.4929072, 2.1452474 });
+    coordenadas.push_back({ 41.4933070, 2.1453852 });
+    coordenadas.push_back({ 41.4939882, 2.1456419 });
+
+    return coordenadas;
+}
+/////////////////////////////////////////////////////////////////////////
+
+
 
 std::vector<Coordinate> CamiSolucio::getCamiCoords(int caminoSeleccionado) {
     std::vector<Coordinate> coordenadas;
