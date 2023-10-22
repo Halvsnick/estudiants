93ed6bd3839254f9a64f5a66c2b89517b52a8b6a
Natàlia Cortés Danés <1668991@uab.cat>
2023-10-22 13:11:59 +0200

    MapaSolucio - tasca 5 començada i classe plantejada falta implementar les funcions getpdis i getcamins

---
+++ LP-OSM-2223-DLL/MapaSolucio.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "PuntDeInteresBase.h"
+#include "Common.h"
+#include <string>
+#include "MapaBase.h"
+#include <vector>
+#include "CamiBase.h"
+
+using namespace std;
+
+class MapaSolucio : public MapaBase
+{
+private:
+
+    
+public:
+
+    void getPdis(std::vector<PuntDeInteresBase*>&)
+    {
+        //fata fer
+    }
+    void getCamins(std::vector<CamiBase*>&)
+    {
+        //falta fer
+    }
+};
